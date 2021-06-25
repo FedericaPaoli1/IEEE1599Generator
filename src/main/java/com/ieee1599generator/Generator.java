8e3c65e6372f67e129881bf75d947304aacd11aa
Federica Paolì <federicapaoli1@studenti.unimi.it>
2021-06-25 18:03:05 +0200

    Insert notes duration

--- src/main/java/com/ieee1599generator/Generator.java
+++ src/main/java/com/ieee1599generator/Generator.java
@@ -3,8 +3,11 @@ package com.ieee1599generator;
 import java.io.File;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
@@ -19,6 +22,8 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -28,10 +33,10 @@ import org.w3c.dom.Element;
  */
 public class Generator {
 
-    public int getMeasuresNumber(int bpm, long pieceLength, String metre) {
+    public int getMeasuresNumber(int bpm, long pieceLength, int[] metre) {
         double bps = (double) bpm / 60;
         System.out.println("bps: " + bps);
-        int beatsNumber = Integer.parseInt(String.valueOf(metre.charAt(0)));
+        int beatsNumber = metre[0];
         System.out.println("Number of beats: " + beatsNumber);
         double oneBeatLength = 1 / bps;
         System.out.println("oneBeatLength: " + oneBeatLength);
@@ -43,6 +48,13 @@ public class Generator {
         return random.nextInt(max - min) + min;
     }
 
+    public double getRandomNote(Map<Double, int[]> notesMap) {
+        Double[] keySetArray = notesMap.keySet().toArray(new Double[notesMap.keySet().size()]);
+        Random random = new Random();
+        int randomIndex = random.nextInt(notesMap.keySet().size());
+        return keySetArray[randomIndex];
+    }
+
     public static void main(String[] args) throws TransformerException {
         Generator g = new Generator();
         // parametri da chiedere all'utente a riga di comando
@@ -51,20 +63,36 @@ public class Generator {
         int minutes = 2;
         String pieceLength = "PT" + minutes + "M";
         int bpm = 108;
-        int num = 4;
-        int den = 4;
-        String metre = num + ":" + den;
+        String metre = 4 + ":" + 4;
+        int[] metreInNumbers = {Integer.parseInt(String.valueOf(metre.charAt(0))), Integer.parseInt(String.valueOf(metre.charAt(2)))};
         int instrumentsNumber = 5;
         int maxNumberOfPlayedNotes = 10;
-        //
+        int maxNumberOfNotesInAChord = 3;
+        int[] minDuration = {32, 1};
+        int[] maxDuration = {1, 1};
 
+        //
         long seconds = Duration.parse(pieceLength).getSeconds();
         System.out.println("Piece length: " + seconds);
-        int measuresNumber = g.getMeasuresNumber(bpm, seconds, metre);
+        int measuresNumber = g.getMeasuresNumber(bpm, seconds, metreInNumbers);
         System.out.println("Measures number: " + measuresNumber);
-        int notesNumber = g.getRandomNumber(1, maxNumberOfPlayedNotes + 1);
-        System.out.println("Notes number: " + notesNumber);
 
+        Map<Double, int[]> notesMap = new TreeMap<>();
+        for (int i = maxDuration[0]; i <= minDuration[0]; i *= 2) {
+            notesMap.put((double) 1 / i, new int[]{i, 1});
+        }
+
+        double noteValueForAMeasure = (double) 1 / metreInNumbers[1];
+        System.out.println("Value of a note in a measure: " + 1 + "/" + metreInNumbers[1] + " = " + noteValueForAMeasure);
+        
+        List<Element> eventsList = new ArrayList<>();
+
+        /* Printing notesMap
+        System.out.println("Notes map keys: " + notesMap.keySet());
+        for(int[] n : notesMap.values()) {
+            System.out.println("Notes map values: " + n[0] + " " + n[1] + "\n");
+        }
+         */
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 
         try {
@@ -99,34 +127,139 @@ public class Generator {
             Element spine = doc.createElement("spine");
             logic.appendChild(spine);
 
+            boolean firstEventsDefined = false;
+
             for (int i = 1; i <= instrumentsNumber; i++) {
                 Element event = doc.createElement("event");
-                event.setAttribute("id", "Instrument_" + i + "_measure1_ev0");
+                event.setAttribute("id", "Instrument_" + i + "_voice0_measure1_ev0");
                 event.setAttribute("timing", "null");
                 event.setAttribute("hpos", "null");
                 spine.appendChild(event);
+                eventsList.add(event);
+                firstEventsDefined = true;
             }
-
+            
             for (int i = 1; i <= instrumentsNumber; i++) {
                 Element event = doc.createElement("event");
                 event.setAttribute("id", "TimeSignature_Instrument_" + i + "_1");
                 event.setAttribute("timing", "null");
                 event.setAttribute("hpos", "null");
                 spine.appendChild(event);
+                eventsList.add(event);
             }
 
             for (int j = 1; j <= measuresNumber; j++) {
-                int randomInstrumentsNumber = g.getRandomNumber(1, instrumentsNumber + 1);
-                for (int k = 1; k <= notesNumber; k++) {
+                System.out.println("MEASURE " + j);
+                int randomInstrumentsNumber = g.getRandomNumber(1, instrumentsNumber + 1); // TODO extract always all instruments, but randomly
+                if (firstEventsDefined) {
                     for (int i = 1; i <= randomInstrumentsNumber; i++) {
-                        Element event = doc.createElement("event");
-                        event.setAttribute("id", "Instrument_" + i + "_measure" + j + "_ev" + k);
-                        event.setAttribute("timing", "null");
-                        event.setAttribute("hpos", "null");
-                        spine.appendChild(event);
+                        int notesNumber = g.getRandomNumber(1, maxNumberOfPlayedNotes + 1);
+                        System.out.println("Notes number for instrument " + i + " : " + notesNumber);
+                        for (int k = 1; k < notesNumber; k++) {
+                            Element event = doc.createElement("event");
+                            event.setAttribute("id", "Instrument_" + i + "_voice0_measure" + j + "_ev" + k);
+                            event.setAttribute("timing", "null");
+                            event.setAttribute("hpos", "null");
+                            spine.appendChild(event);
+                            eventsList.add(event);
+                        }
+                    }
+                    firstEventsDefined = false;
+                } else {
+                    for (int i = 1; i <= randomInstrumentsNumber; i++) {
+                        int notesNumber = g.getRandomNumber(1, maxNumberOfPlayedNotes + 1);
+                        System.out.println("Notes number for instrument " + i + " : " + notesNumber);
+                        for (int k = 0; k < notesNumber; k++) {
+                            Element event = doc.createElement("event");
+                            event.setAttribute("id", "Instrument_" + i + "_voice0_measure" + j + "_ev" + k);
+                            event.setAttribute("timing", "null");
+                            event.setAttribute("hpos", "null");
+                            spine.appendChild(event);
+                            eventsList.add(event);
+                        }
+                    }
+
+                }
+            }
+
+            Element los = doc.createElement("los");
+            logic.appendChild(los);
+
+            Element staffList = doc.createElement("staff_list");
+            los.appendChild(staffList);
+
+            for (int i = 1; i <= instrumentsNumber; i++) {
+                Element staff = doc.createElement("staff");
+                staff.setAttribute("id", "Instrument_" + i + "_staff");
+                staff.setAttribute("line_number", "5");
+                staffList.appendChild(staff);
+
+                Element timeSignature = doc.createElement("time_signature");
+                timeSignature.setAttribute("event_ref", "TimeSignature_Instrument_" + i + "_1");
+                staff.appendChild(timeSignature);
+                Element timeIndication = doc.createElement("time_indication");
+                timeIndication.setAttribute("den", "" + metreInNumbers[1]);
+                timeIndication.setAttribute("num", "" + metreInNumbers[0]);
+                timeSignature.appendChild(timeIndication);
+
+                Element part = doc.createElement("part");
+                part.setAttribute("id", "Instrument_" + i);
+                los.appendChild(part);
+
+                Element voiceList = doc.createElement("voice_list");
+                part.appendChild(voiceList);
+                Element voiceItem = doc.createElement("voice_item");
+                voiceItem.setAttribute("id", "Instrument_" + i + "_0_voice");
+                voiceItem.setAttribute("staff_ref", "Instrument_" + i + "_staff");
+                voiceList.appendChild(voiceItem);
+
+                for (int j = 1; j <= measuresNumber; j++) {
+                    Element measure = doc.createElement("measure");
+                    measure.setAttribute("number", "" + j);
+                    part.appendChild(measure);
+
+                    Element voice = doc.createElement("voice");
+                    voice.setAttribute("voice_item_ref", "Instrument_" + i + "_0_voice");
+                    measure.appendChild(voice);
+
+                    String s = "Instrument_" + i + "_voice0_measure" + j + "_";
+                    var eventsNumber = eventsList.stream().filter(e -> e.getAttribute("id").contains(s)).count();
+                    System.out.println(s);
+                    System.out.println(eventsNumber);
+                    
+                    for (int k = 0; k < eventsNumber; k++) {
+
+                        Element chord = doc.createElement("chord");
+                        chord.setAttribute("event_ref", "Instrument_" + i + "_voice0_measure" + j + "_ev" + k);
+                        voice.appendChild(chord);
+
+                        double randomNote = g.getRandomNote(notesMap);
+                        while (randomNote * metreInNumbers[0] > 1) {
+                            randomNote = g.getRandomNote(notesMap);
+                        }
+
+                        if (randomNote == 1) {
+                            Element duration = doc.createElement("duration");
+                            duration.setAttribute("den", "" + metreInNumbers[1]);
+                            duration.setAttribute("num", "" + metreInNumbers[0]);
+                            chord.appendChild(duration);
+                        } else {
+                            Element duration = doc.createElement("duration");
+                            duration.setAttribute("den", "" + notesMap.get(randomNote)[0]);
+                            duration.setAttribute("num", "" + 1);
+                            chord.appendChild(duration);
+                        }
+                        int randomNotesNumberInAChord = g.getRandomNumber(1, maxNumberOfNotesInAChord + 1);
+                        /*for (int k = 1; k <= randomNotesNumberInAChord; k++) {
+                        // k: number of noteheads
+                    }*/
+
                     }
                 }
             }
+            
+            // TODO
+            // insert chords and rests
 
             // salvataggio
             Result output = new StreamResult(new File("ieee1599.xml"));
